85419079928cb83e8bae5c05f383e2f139c8ebea
Jonathan Frederic <jon.freder@gmail.com>
2016-03-01 17:06:53 -0800

    Make the search input a bootstrap control

--- docs/source/_static/index.js
+++ docs/source/_static/index.js
@@ -2,4 +2,6 @@ window.onload = function() {
   Array.prototype.forEach.call(document.querySelectorAll('a'), function (a) {
     a.href = a.href.replace(/\.md$/, '.html');
   });
+  
+  document.querySelector('form input[type=text]').className = 'form-control input-lg';
 };
