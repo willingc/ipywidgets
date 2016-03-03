aea7cd124dcd4df650ba817d9fc5b140e66bfd32
Jonathan Frederic <jon.freder@gmail.com>
2016-03-03 13:01:06 -0800

    jp theme

--- docs/source/_static/index.js
+++ docs/source/_static/index.js
@@ -1,8 +1,11 @@
 window.onload = function() {
+  // TODO: See if md parsing conf can do this, or extension (compile time)
   Array.prototype.forEach.call(document.querySelectorAll('a'), function (a) {
-    a.href = a.href.replace(/\.md$/, '.html');
+    a.href = a.href
+      .replace(/\.md$/, '.html')
+      .replace(/\.ipynb$/, '.html');
   });
-  
+
   document.querySelector('form input[type=text]').className = 'form-control input-lg';
   
   // TODO: Put this in the template
---
+++ docs/source/index.rst
@@ -0,0 +1,12 @@
+ipywidgets
+==========
+
+Contents
+--------
+
+.. toctree::
+    :maxdepth: 2
+
+    contributing.md
+    dev_install.md
+    test
